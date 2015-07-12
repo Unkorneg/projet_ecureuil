3796d2108da143f192e264315b5d10c82c514818
Alexein41 <alexein41@hotmail.fr>
2015-07-12 22:28:24 +0200

    correction sub-menu-item et menu-item + update js

--- js/affichage-always.js
+++ js/affichage-always.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
 
+	// réglage du margin-top du main
+
 	var mrgntop = $('header').outerHeight(true);
 
     $('#main').css({
         'margin-top' : mrgntop
     });
+
+    // réglage de l'apparition des petits trucs dans le menu
+    /*document.getElementById("demo").addEventListener("mouseover", mouseOver);
+	document.getElementById("demo").addEventListener("mouseout", mouseOut);
+
+	function mouseOver() {
+	    document.getElementById("demo").style.color = "red";
+	}
+
+	function mouseOut() {
+    	document.getElementById("demo").style.color = "black";
+	}*/
+
+
 });
