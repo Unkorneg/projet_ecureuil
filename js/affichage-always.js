c8be8fe7531ed9e3986823a3f42a8c3c92ae458d
Alexein41 <alexein41@hotmail.fr>
2015-07-13 22:34:31 +0200

    Augmentation clarté du fichier affichage-always.js

--- js/affichage-always.js
+++ js/affichage-always.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
 	// réglage du margin-top du main
-
 	var mrgntop = $('header').outerHeight(true);
 
 	$('#main').css({
@@ -17,53 +16,43 @@ $(document).ready(function(){
 
     // réglage de l'apparition des petits trucs dans le menu
     $(".sub-menu-item").hide();
+
     $(".menu-item").hover(
     	function() {
-
     		var nbEnfants = $(this).children().children().length;
 
     		if (nbEnfants) {
-
     			var aHeight = $(this).children("a").outerHeight(true);
     			var subHeight = $(this).children().children().outerHeight(true);
     			var futureLongueurBloc = subHeight*nbEnfants + aHeight;
-
     			var futureLongueurAutresBlocs = (espaceDispoMenu - futureLongueurBloc)/(nbEnfantsMenu - 1);
-    			console.log(futureLongueurAutresBlocs);
 
     			$(this).animate({
     				'height' : futureLongueurBloc
     			},300);
-
     			$(".menu-item").not($(this)).animate({
     				'height' : futureLongueurAutresBlocs
     			},300);
-
     			$(this).children().children().css({
     				'height' : subHeight
     			});
 
     			$(this).children().children().slideDown(500);
-
     		}
-    	}, function() {
 
+    	}, function() {
     		var nbEnfants = $(this).children().children().length;
 
     		if (nbEnfants) {
-
     			$(this).children().children().slideUp(500);
 
     			var espaceDispoMenu = window.innerHeight - $('header').outerHeight(true);
     			var nbEnfantsMenu = $("#menu-principal").children().length;
     			var heightMenuItem = espaceDispoMenu/nbEnfantsMenu;
+
     			$(".menu-item").animate({
     				'height' : heightMenuItem
     			}, 300);
-
     		}
-
-
     	});
 });
-
