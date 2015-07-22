3d4c100420322fc3fcc7099b6bbf85199d0f494d
Alexein41 <alexein41@hotmail.fr>
2015-07-22 22:36:01 +0200

    le menu ne danse plus mais il reste à améliorer
    il faut maintenant que : après que je suis passé sur une case de menu,
    quand je vais sur une autre case, elle se mette en route. actuellement,
    ça ne fait rien
    envisager de faire un compteur qui compte le temps (comme avant)
    
    les commentaires du précédent code ont été laissés mais vont être
    enlevés

--- js/affichage-always.js
+++ js/affichage-always.js
@@ -7,7 +7,6 @@ $(document).ready(function(){
 		'margin-top' : mrgntop
 	});
 
-
 	var espaceDispoMenu = window.innerHeight - $('header').outerHeight(true);
 	var margBottom = parseInt($(".menu-item").css("marginBottom"),10);
 	var nbEnfantsMenu = $("#menu-principal").children().length;
@@ -23,38 +22,143 @@ $(document).ready(function(){
         'height' : 0
     });
 
-    $(".menu-item").hover(
-    	function() {
+    var peutOnDeroulerLesMenus = true;
 
-    		var nbEnfants = $(this).children().children().length;
+    $(".menu-item").mouseenter(function() {
 
-    		if (nbEnfants) {
+        $this = $(this);
+        var nbEnfants = $this.children().children().length;
 
-                $(".menu-item").not($(this)).addClass("bloque");
+        if (nbEnfants && peutOnDeroulerLesMenus && !($(".animating").length)) {
 
-                if(!$(this).hasClass("bloque")) {
-                    var aHeight = $(this).children("a").outerHeight(true);
-                    var futureLongueurBloc = heightSubMenuItem * nbEnfants + aHeight;
-                    var futureLongueurAutresBlocs = (espaceDispoMenu - futureLongueurBloc)/(nbEnfantsMenu - 1);
+            $(this).addClass("animating");
 
-                    $(this).animate({
-                        'height' : futureLongueurBloc
-                    },300);
-                    $(".menu-item").not($(this)).animate({
-                        'height' : futureLongueurAutresBlocs
-                    },300);
-                    $(this).children().children().animate({
-                        'height' : heightSubMenuItem
-                    },300);
+            peutOnDeroulerLesMenus = false;
+
+            var aHeight = $this.children("a").outerHeight(true);
+            var futureLongueurBloc = heightSubMenuItem * nbEnfants + aHeight;
+            var futureLongueurAutresBlocs = (espaceDispoMenu - futureLongueurBloc)/(nbEnfantsMenu - 1);
+
+            $this.animate({
+                'height' : futureLongueurBloc
+            },300);
+            $(".menu-item").not($(this)).animate({
+                'height' : futureLongueurAutresBlocs
+            },300);
+            $this.children().children().animate({
+                'height' : heightSubMenuItem
+            },300);
+          
+        }
+
+    });
+
+    $(".menu-item").mouseleave(function() {
+
+    $this = $(this);
+    var nbEnfants = $this.children().children().length;
+
+        if (nbEnfants && $(this).hasClass("animating")) {
+
+            $(".menu-item").animate({
+                'height' : heightMenuItem
+            },300);
+            $this.children().children().animate({
+                'height' : 0
+            },300);
+
+            $(".animating").removeClass("animating");
+            setTimeout(function() {
+                peutOnDeroulerLesMenus = true;
+            },650);
+        }
+
+    });
+
+
+
+
+
+
+
+
+    //var compteur = 20; //en millisecondes; compteur n'est pas initialement = à 0 au cas où
+
+    /*$(".menu-item").each(function() {
+
+        $this = $(this);
+
+        $this.mouseenter(function() {
+            var nbEnfants = $(this).children().children().length;
+
+            if (nbEnfants) {
+
+                if ($("enAnimation").length == 0) {
+
+                $this.addClass("animated"); // l'animation d'ouverture (mouseover seulement)
+                $this.addClass("enAnimation"); //c'est en train d'être animé
+                var aHeight = $this.children("a").outerHeight(true);
+                var futureLongueurBloc = heightSubMenuItem * nbEnfants + aHeight;
+                var futureLongueurAutresBlocs = (espaceDispoMenu - futureLongueurBloc)/(nbEnfantsMenu - 1);
+
+                setInterval(function() {
+                    compteur = compteur + 20;
+                },20)
+
+                $this.animate({
+                    'height' : futureLongueurBloc
+                },300);
+                $(".menu-item").not($(this)).animate({
+                    'height' : futureLongueurAutresBlocs
+                },300);
+                $this.children().children().animate({
+                    'height' : heightSubMenuItem
+                },300);
+                setTimeout(function(){
+                    $this.removeClass("animated");
+                },300);
+
+                }
+                else {
+
+                    setTimeout(function() {
+
+                $this.addClass("animated"); // l'animation d'ouverture (mouseover seulement)
+                $this.addClass("enAnimation"); //c'est en train d'être animé
+                var aHeight = $this.children("a").outerHeight(true);
+                var futureLongueurBloc = heightSubMenuItem * nbEnfants + aHeight;
+                var futureLongueurAutresBlocs = (espaceDispoMenu - futureLongueurBloc)/(nbEnfantsMenu - 1);
+
+                setInterval(function() {
+                    compteur = compteur + 20;
+                },20)
+
+                $this.animate({
+                    'height' : futureLongueurBloc
+                },300);
+                $(".menu-item").not($(this)).animate({
+                    'height' : futureLongueurAutresBlocs
+                },300);
+                $this.children().children().animate({
+                    'height' : heightSubMenuItem
+                },300);
+                setTimeout(function(){
+                    $this.removeClass("animated");
+                },300);
+
+                    }, 310);
+
                 }
 
             }
 
-        }, function() {
+        });
 
-          var nbEnfants = $(this).children().children().length;
+        $this.mouseleave(function() {
+            var nbEnfants = $this.children().children().length;
 
-          if (nbEnfants && !$(this).hasClass("bloque")) {
+                if (nbEnfants) {
+
                 var espaceDispoMenu = window.innerHeight - $('header').outerHeight(true);
                 var margBottom = parseInt($(".menu-item").css("marginBottom"),10);
                 var nbEnfantsMenu = $("#menu-principal").children().length;
@@ -64,12 +168,68 @@ $(document).ready(function(){
                 $(".menu-item").animate({
                     'height' : heightMenuItem
                 },300);
-                $(this).children().children().animate({
+                $this.children().children().animate({
                     'height' : 0
-                },300, function() {
-                    $(".menu-item").removeClass("bloque");
-                });
-
+                },300);
+                setTimeout(function() {
+                    $this.removeClass("animated"); //c'est pas grave si on le fait même si c'est déjà fait
+                    $this.removeClass("enAnimation");
+                    compteur = 20;
+                }, 300);
             }
+
         });
+
+});*/
+
+
+    /*$(".menu-item").mouseenter(function() {
+        $(".menu-item").addClass("bloque");
+        var nbEnfants = $(this).children().children().length;
+
+        if (nbEnfants) {
+
+            if(!($(this).hasClass("bloque"))) {
+                var aHeight = $(this).children("a").outerHeight(true);
+                var futureLongueurBloc = heightSubMenuItem * nbEnfants + aHeight;
+                var futureLongueurAutresBlocs = (espaceDispoMenu - futureLongueurBloc)/(nbEnfantsMenu - 1);
+
+                $(this).animate({
+                    'height' : futureLongueurBloc
+                },300);
+                $(".menu-item").not($(this)).animate({
+                    'height' : futureLongueurAutresBlocs
+                },300);
+                $(this).children().children().animate({
+                    'height' : heightSubMenuItem
+                },300);
+            }
+        }
+
+    });
+
+    $(".menu-item").mouseleave(function() {
+        var nbEnfants = $(this).children().children().length;
+
+        if (nbEnfants && !($(this).hasClass("bloque"))) {
+
+            var espaceDispoMenu = window.innerHeight - $('header').outerHeight(true);
+            var margBottom = parseInt($(".menu-item").css("marginBottom"),10);
+            var nbEnfantsMenu = $("#menu-principal").children().length;
+            espaceDispoMenu = espaceDispoMenu - nbEnfantsMenu*margBottom;
+            var heightMenuItem = espaceDispoMenu/nbEnfantsMenu;
+
+            setTimeout(function() {
+                $(".menu-item").removeClass("bloque");
+            }, 300);
+            $(".menu-item").animate({
+                'height' : heightMenuItem
+            },300);
+            $(this).children().children().animate({
+                'height' : 0
+            },300);
+
+        }
+    });*/
+
 });
